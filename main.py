8a4c93d35275c2e5bf231c773637f61574b6a12a
CtrlZExpert <vernett.cartwright@gmail.com>
2024-11-29 00:50:23 -0500

    A: day 1 asteroid project

---
+++ main.py
@@ -0,0 +1,31 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+from player import Player
+
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
+
+
+if __name__ == "__main__":
+    main()
