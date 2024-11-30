94f37f2c14b8c404d851aead5cb2ae738fd44c3d
CtrlZExpert <vernett.cartwright@gmail.com>
2024-11-30 09:51:01 -0500

    B: Add asteroid, collision, shoots

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroids_field = AsteroidField()
+
+
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+
+
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -19,12 +36,26 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
+        
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
-
+        
+        
 
 
 if __name__ == "__main__":
